672c37c6ff16205b90cfa7958c50fbf73b91e8d0
Shreeyanka Sahoo <9836shreeyanka@gmail.com>
2026-09-07 12:46:37 +0530

    Update test readings and comments in test_shelf_detector

--- test_shelf_detector.py
+++ test_shelf_detector.py
@@ -16,14 +16,26 @@ print("READING 1")
 print(detector.update(reading_1))
 
 
-# Second reading: rack_1 distance increased by 2 cm.
-# This should trigger the stock alert.
+# Second reading: no change
 reading_2 = {
+    "rack_1": 14.2,
+    "rack_2": 28.5,
+    "rack_3": -1.0,
+    "rack_4": 12.0,
+}
+
+print("\nREADING 2 - NO CHANGE")
+print(detector.update(reading_2))
+
+
+# Third reading: rack_1 increased by 2 cm
+# This should trigger the stock alert.
+reading_3 = {
     "rack_1": 16.2,
     "rack_2": 28.5,
     "rack_3": -1.0,
     "rack_4": 12.0,
 }
 
-print("\nREADING 2")
-print(detector.update(reading_2))
+print("\nREADING 3 - STOCK REMOVED")
+print(detector.update(reading_3))
